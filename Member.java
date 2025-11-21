e14c96f249a71d8ed99ce085b294f944ad69b2d9
Filum1 <135311078+Filum1@users.noreply.github.com>
2025-11-21 16:38:47 +0300

    Added Setters and Getters

--- Member.java
+++ Member.java
@@ -1,1 +1,27 @@
 public class Member {
+
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public int getId() {
+    return id;
+  }
+  public void setBorrowedCount(int borrowedCount) {
+    this.borrowedCount = borrowedCount;
+  }
+  public int getBorrowedCount() {
+    return borrowedCount;
+  }
+  public void setSessionFees(double sessionFees) {
+    this.sessionFees = sessionFees;
+  }
+  public double getSessionFees() {
+    return sessionFees;
+}
